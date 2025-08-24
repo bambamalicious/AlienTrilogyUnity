861ce1df275ee31002eaa668d405f6564541c2a9
bambamalicious <82620887+bambamalicious@users.noreply.github.com>
2025-08-24 16:24:22 -0300

    Add files via upload

---
+++ ATU Map Viewer/Assets/pathObj.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class pathObj : MonoBehaviour
+{
+    public string Name;
+    public byte X;
+    public byte Y;
+    public byte nodeState;
+    public byte NodeA;
+    public byte NodeB;
+    public byte NodeC;
+    public byte NodeD;
+}
